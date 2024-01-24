b7ddf675b1d2558bb866f1f65ba0a1ccdf699f9f
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-24 19:39:00 +0100

    Add project files.

---
+++ Zadanie3Warunki/Program.cs
@@ -0,0 +1,19 @@
+﻿int number;
+
+Console.WriteLine("Wprowadź liczbę: ");
+var enteredNumber = Console.ReadLine();
+
+Int32.TryParse(enteredNumber, out number);
+
+
+if (number > 0)
+{
+    Console.WriteLine($"{number} jest liczbą dodatnią");
+}
+else if(number == 0){
+    Console.WriteLine($"{number} nie jest liczbą dodatnią ani liczbą ujemną");
+}
+else
+{
+    Console.WriteLine($"{number} jest liczbą ujemną");
+}
