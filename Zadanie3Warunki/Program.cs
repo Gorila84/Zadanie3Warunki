b92f04f2a243eb6d48c25d3fe174c0796d4cf1e8
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-30 21:54:29 +0100

    improving the return variable and improving code cleanliness

--- Zadanie3Warunki/Program.cs
+++ Zadanie3Warunki/Program.cs
@@ -1,16 +1,17 @@
-﻿int number;
+﻿
 
 Console.WriteLine("Wprowadź liczbę: ");
 var enteredNumber = Console.ReadLine();
 
-Int32.TryParse(enteredNumber, out number);
+Int32.TryParse(enteredNumber, out int number);
 
 
 if (number > 0)
 {
     Console.WriteLine($"{number} jest liczbą dodatnią");
 }
-else if(number == 0){
+else if(number == 0)
+{
     Console.WriteLine($"{number} nie jest liczbą dodatnią ani liczbą ujemną");
 }
 else
